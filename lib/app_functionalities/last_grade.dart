58919307e6c04572ef2a6e501bf6ba93b90c9050
Lovro Dijanović (Span) <ldijanovic@span.eu>
2022-09-12 15:13:29 +0200

    small fixes

--- lib/app_functionalities/last_grade.dart
+++ lib/app_functionalities/last_grade.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:ss_donji_miholjac/models/my_text_button.dart';
+import 'package:ss_donji_miholjac/models/text_block.dart';
 import '../models/functionality_screen.dart';
 
 class LastGrade extends StatelessWidget {
@@ -9,7 +11,11 @@ class LastGrade extends StatelessWidget {
     return FunctionalityScreen(
         pageTitle: 'Završni razredi',
         child: ListView(
-          children: const [],
+          children: const [
+            TextBlock('Dragi učenici, na likovima ispod možete pronaći informacije vezane za državnu maturu i završne ispite.'),
+            MyTextButton('Državna matura', 'http://ss-donji-miholjac.skole.hr/skola/dr_avna_matura_informacije'),
+            MyTextButton('Završni ispiti', 'http://ss-donji-miholjac.skole.hr/skola/dr_avna_matura_informacije/zavr_ni_ispiti')
+          ],
         ));
   }
 }
--- lib/app_functionalities/scedule.dart
+++ lib/app_functionalities/scedule.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:ss_donji_miholjac/models/my_text_button.dart';
+import 'package:ss_donji_miholjac/models/text_block.dart';
+import '../models/functionality_screen.dart';
 
 class Scedule extends StatelessWidget {
   const Scedule({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Text('scedule');
+    return FunctionalityScreen(
+        pageTitle: 'Raspored',
+        child: ListView(
+          children: const [
+            TextBlock('Dragi učenici, raspored sati možete pronaći pritiskom na link ispod'),
+            MyTextButton('Raspored', 'https://sites.google.com/view/rasporedsdm/kalendar-%C5%A1kole')
+          ],
+        ));
   }
 }
